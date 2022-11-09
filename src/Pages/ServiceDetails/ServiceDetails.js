e37f8bd8bc32a6fd3b13b762632ffde6057c7f36
mdansarulislam641 <mdansarulislam641.com>
2022-11-09 09:02:51 +0600

    review fetch api and create

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -12,7 +12,6 @@ const ServiceDetails = () => {
         .then(res=>res.json())
         .then(data=>setDetails(data))
     },[id])
-    console.log(details)
 
     const handleReview = event =>{
         event.preventDefault()
@@ -24,8 +23,8 @@ const ServiceDetails = () => {
     }
 
     return (
-      <div className=' py-10 '>
-        <div className='text-center lg:text-5xl md:text-3xl text-2xl font-extrabold font-mono text-white my-5 mx-2'>
+      <div className=' py-10 bg-gray-500'>
+        <div className='text-center lg:text-5xl md:text-3xl  text-2xl font-extrabold font-mono text-white my-5 mx-2'>
             <h4>WellCome {name} Details Page</h4>
         </div>
          <div className='max-w-[1300px] mx-auto'>
@@ -47,21 +46,23 @@ const ServiceDetails = () => {
          </div>
          </section>
          {/* review section */}
-         <section>
-            <h1>Review This Service</h1>
-            <div>
+         <section className='my-24'>
+            <h1 className='text-center text-white font-extrabold font-mono lg:text-4xl text-2xl'>Review This Service</h1>
+            <div className='text-center mx-5'>
                 <form onSubmit={handleReview}>
-                    <div>
-                   <input type="text" name='name' placeholder='Your Name' id="" required />
+                   <div className='lg:flex gap-5 items-center justify-center'>
+                   <div className='border-2  lg:w-1/2 w-full my-3  rounded-lg'>
+                   <input className='w-full py-2 bg-gray-200 text-2xl px-5' type="text" name='name' placeholder='Your Name' id="" required />
                     </div>
-                    <div>
-                    <input type="text" name='rating' placeholder='Your Ratings' id="" />
+                    <div className='border-2 lg:w-1/2 w-full rounded-lg'>
+                    <input className='w-full py-2 bg-gray-200 text-2xl px-5' type="text" name='rating' placeholder='Your Ratings' id="" />
                     </div>
-                    <div>
-                    <textarea name="yourMessage" id="" cols="30" rows="5" required></textarea>
+                   </div>
+                    <div className='my-2'>
+                    <textarea className='w-full my-2 py-2 text-red-900 bg-gray-200 text-2xl px-5 resize-none' name="yourMessage" placeholder='Your Message Here....' id="" cols="30" rows="5" required></textarea>
                     </div>
-                    <div>
-                        <button className="btn btn-primary" type='submit'>Submit Review</button>
+                    <div className='text-center my-5'>
+                        <button className="btn btn-primary px-10" type='submit'>Submit Review</button>
                     </div>
                 </form>
             </div>
